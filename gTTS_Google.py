935a9be9606726afa6fce16bfeb36d521871ba9d
Nhat Bui <minhnhat@linuxmail.org>
2022-06-22 19:31:24 +0700

    Update gTTS_Google.py

--- gTTS_Google.py
+++ gTTS_Google.py
@@ -10,29 +10,29 @@ def speak(text):
     tts.save(filename)
     playsound(filename)
 
-#main code
-word = ""
-text = ""
-
-tone_marks = ["<", ">", "?", "~", "."] #sắc, huyền, hỏi, ngã, nặng
-
-braille_encode = ['1', ' ', ' ', ' ', ' ', ' ', '345', ' ', ' ', ' ', ' ', ' ', '16', ' ', ' ', ' ', ' ', ' ', '12', '14', '145', '2346',
-                '15', ' ', ' ', ' ', ' ', ' ', '126', ' ', ' ', ' ', ' ', ' ', '124', '1245', '125', '24', ' ', ' ', ' ', ' ', ' ', '245', '13', 
-                '123', '134', '1345', '135', ' ', ' ', ' ', ' ', ' ', '1456', ' ', ' ', ' ', ' ', ' ', '246', ' ', ' ', ' ', ' ', ' ', '1234', '12345',
-                '1235', '234', '2345', '136',  ' ', ' ', ' ', ' ', ' ','1256', ' ', ' ', ' ', ' ', ' ', '1236', '2456', '1346', '13456', ' ', ' ', ' ', ' ', ' ', '1356']
-
-tone_marks_encode = ['35', '56', '26', '36', '6']
+   
+braille_encode = {
+    '1': 'a', '12': 'b', '14': 'c', '145': 'd', '15': 'e', '124': 'f',
+    '1245': 'g', '125': 'h', '24': 'i', '245': 'j', '13': 'k', '123': 'l', '134': 'm',
+    '1345': 'n', '135': 'o', '1234': 'p', '12345': 'q', '1235': 'r', '234': 's', '2345': 't',
+    '136': 'u', '1236': 'v', '2456': 'w', '1346': 'x', '13456': 'y', '1356': 'z',
+    '345': 'ă', '16': 'â', '2346': 'đ', '126': 'ê', '1456': 'ô', '246': 'ơ', '1256': 'ư',
+    '35': 'sắc', '56': 'huyền', '26': 'hỏi', '36': 'ngã', '6': 'nặng'}
+}
                 
 alphabet = ['a', 'à', 'ả', 'ã', 'á', 'ạ', 'ă', 'ằ', 'ẳ', 'ẵ', 'ắ', 'ặ', 'â', 'ầ', 'ẩ', 'ẫ', 'ấ', 'ậ', 'b', 'c', 'd', 'đ', 
         'e', 'è', 'ẻ', 'ẽ', 'é', 'ẹ', 'ê', 'ề', 'ể', 'ễ', 'ế', 'ệ', 'f', 'g', 'h', 'i', 'ì', 'ỉ', 'ĩ', 'í', 'ị', 'j', 'k', 
         'l', 'm', 'n', 'o', 'ò', 'ỏ', 'õ', 'ó', 'ọ', 'ô', 'ồ', 'ổ', 'ỗ', 'ố', 'ộ', 'ơ', 'ờ', 'ở', 'ỡ', 'ớ', 'ợ', 'p', 'q', 
         'r', 's', 't', 'u', 'ù', 'ủ', 'ũ', 'ú', 'ụ', 'ư', 'ừ', 'ử', 'ữ', 'ứ', 'ự', 'v', 'w', 'x', 'y', 'ỳ', 'ỷ', 'ỹ', 'ý', 'ỵ', 'z']
 
+
+word = ""
+text = ""
 func = ['space', 'word']
 
 while True:
-    inp = input("Nhap: ")
-
+    inp = input("Nhap: ").lower()
+    
     if (inp == 'q'):
         speak("Chào tạm biệt!")
         break
@@ -42,9 +42,12 @@ while True:
     elif (inp == 'space'):
         word = word + " "
         speak("Dấu cách")
+        
     elif (inp == 'word'):
         tam = word
-        for i in range(0,len(tam)):
+        n = len(tam)
+        
+        for i in range(0, n):
             if (tam[i] == "<"):
                 char_index = alphabet.index(tam[i+1])
                 temp = tam[i] + tam[i+1]
@@ -72,31 +75,22 @@ while True:
                 word = tam.replace(temp, temp_replaced)
         print(word)
         speak(word)
-    else:
-        if (inp in braille_encode):
-            index = braille_encode.index(inp)
-            word = word + alphabet[index]
-            if (alphabet[index] == 'i'):
-                text = "Chữ " + alphabet[index] + " ngắn"
-            elif (alphabet[index] == 'y'):
-                text = "Chữ " + alphabet[index] + " dài"
+        
+    else:  # for letters
+        if (inp in braille_encode.values()):
+            letter = braille_encode[inp]
+            word = word + letter
+            
+            if (letter == 'i'):
+                text = "Chữ i ngắn"
+            elif (letter == 'y'):
+                text = "Chữ y dài"
             else:
-                text = "Chữ " + alphabet[index]
-        #####################################################       
-        elif (inp in tone_marks_encode):
-            index = tone_marks_encode.index(inp)
-            word = word + tone_marks[index]
-            if (tone_marks[index] == "<"):
-                text = "Dấu sắc"
-            elif (tone_marks[index] == ">"):
-                text = "Dấu huyền"
-            elif (tone_marks[index] == "?"):
-                text = "Dấu hỏi"
-            elif (tone_marks[index] == "~"):
-                text = "Dấu ngã"
-            elif (tone_marks[index] == "."):
-                text = "Dấu nặng"
-        #####################################################
+                if letter in ["<", ">", "?", "~", "."]:
+                    text = "Dấu " + letter
+                else:
+                    text = "Chữ " + letter
+                    
         else:
             text = "Ký tự không tồn tại"
         speak(text)
